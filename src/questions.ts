176f410cf34b02a15889d9330fb29a33f9e43d29
Dougo16 <zacd1302@gmail.com>
2026-09-18 01:40:29 +1000

    Implement questionnaire state machine

--- src/questions.ts
+++ src/questions.ts
@@ -22,6 +22,14 @@ export type AskUserInput = Static<typeof AskUserInputSchema>;
 export type QuestionInput = Static<typeof QuestionSchema>;
 export type OptionInput = Static<typeof OptionSchema>;
 
+export interface Answer {
+	questionId: string;
+	value: string;
+	label: string;
+	note?: string;
+	isOther: boolean;
+}
+
 export interface NormalizedQuestion extends QuestionInput {
 	allowOther: boolean;
 	initialOptionIndex: number;
